31c989a902f5a0c42de68a8e83382a66c21c7482
Andrey Koval <andreykoval@MacBook-Pro-Andrey.local>
2023-09-14 12:59:51 +0300

    init: app to do list

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import "./app.css";
+import { Home } from "./pages";
+
+function App() {
+  return <h1>work</h1>;
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+  },
+  target: "web",
+  devServer: {
+    port: "3000",
+    static: ["./public"],
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env", //compiling ES2015+ syntax
+              "@babel/preset-react", //for react
+            ],
+            plugins: ["@babel/plugin-transform-runtime"],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+              modules: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
