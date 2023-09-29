c5e5fe135b48ec507b0342e497d504b00664e640
Andrey Koval <andreykoval@MacBook-Pro-Andrey.local>
2023-09-29 12:37:26 +0300

    fix: scroll

--- src/pages/TodoDetails/TodoDetails.tsx
+++ src/pages/TodoDetails/TodoDetails.tsx
@@ -49,7 +49,46 @@ const TodoDetails = () => {
   }
 
   return (
-    <div className={styles.container}>
+    <div className={styles.cardContainer}>
+      <div className={styles.card}>
+        <div className={styles.header}>
+          <h1 className={styles.title}>Todo details</h1>
+          <p className={styles.subTitle}>
+            <span>id:</span>
+            {todo?.id}
+          </p>
+        </div>
+
+        <div className={styles.valueSection}>
+          <div className={styles.editBox}>
+            <p>Value:</p>
+            <button
+              className={styles.editButton}
+              onClick={() => setIsOpenModal(true)}
+            >
+              Edit
+            </button>
+          </div>
+          <div className={styles.valueBox}>
+            <p className={styles.valueText}>{todo?.value}</p>
+          </div>
+        </div>
+
+        <div className={styles.dateSection}>
+          <p className={styles.subTitle}>
+            <span>date: </span>
+            {todo?.date.toLocaleString('ru')}
+          </p>
+        </div>
+        <div className={styles.footer}>
+          <button
+            onClick={() => navigate(ROUTS.HOME)}
+            className={styles.goBackButton}
+          >
+            Go back
+          </button>
+        </div>
+      </div>
       {isOpenModal && (
         <EditTodoModal
           closeModal={closeModal}
@@ -57,47 +96,6 @@ const TodoDetails = () => {
           dataModal={todo}
         />
       )}
-      <div className={styles.cardContainer}>
-        <div className={styles.card}>
-          <div className={styles.header}>
-            <h1 className={styles.title}>Todo details</h1>
-            <p className={styles.subTitle}>
-              <span>id:</span>
-              {todo?.id}
-            </p>
-          </div>
-
-          <div className={styles.valueSection}>
-            <div className={styles.editBox}>
-              <p>Value:</p>
-              <button
-                className={styles.editButton}
-                onClick={() => setIsOpenModal(true)}
-              >
-                Edit
-              </button>
-            </div>
-            <div className={styles.valueBox}>
-              <p className={styles.valueText}>{todo?.value}</p>
-            </div>
-          </div>
-
-          <div className={styles.dateSection}>
-            <p className={styles.subTitle}>
-              <span>date: </span>
-              {todo?.date.toLocaleString('ru')}
-            </p>
-          </div>
-          <div className={styles.footer}>
-            <button
-              onClick={() => navigate(ROUTS.HOME)}
-              className={styles.goBackButton}
-            >
-              Go back
-            </button>
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
