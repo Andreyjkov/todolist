7cee45681e6a5876ef6e31481f366a4781aa0607
Andrey Koval <andreykoval@MacBook-Pro-Andrey.local>
2023-10-20 15:34:49 +0300

    feat: axios

--- src/businessService/apiService.ts
+++ src/businessService/apiService.ts
@@ -1,83 +1,30 @@
 /* eslint-disable no-console */
+import axios from 'axios';
 import { ITodoData } from '@/type/ITodoData';
 
 const SERVER_URL = 'http://localhost:3001/todos';
 
-enum RES_TODO_ERROR {
-  FETCH = 'Error when fetching todos',
-  ADD = 'Error when adding a todo',
-  EDIT = 'Error when editing a todo',
-  DELETE = 'Error when deleting a todo',
-  GET_BY_ID = 'Error when fetching todo by ID',
-}
-
-const handleResponse = (response: Response) => {
-  if (!response.ok) {
-    throw new Error(`HTTP error! Status: ${response.status}`);
-  }
-  return response;
-};
-
 const fetchTodos = async (): Promise<ITodoData[]> => {
-  try {
-    const response = await fetch(SERVER_URL);
-    return await handleResponse(response).json();
-  } catch (error) {
-    console.error(RES_TODO_ERROR.FETCH, error);
-  }
+  return (await axios.get(SERVER_URL)).data;
 };
 
 const addTodo = async (newTodo: ITodoData): Promise<ITodoData> => {
-  try {
-    const response = await fetch(SERVER_URL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(newTodo),
-    });
-    return await handleResponse(response).json();
-  } catch (error) {
-    console.error(RES_TODO_ERROR.ADD, error);
-  }
+  return (await axios.post(SERVER_URL, newTodo)).data;
 };
 
 const editTodo = async (
   id: number,
   updatedTodo: ITodoData
-): Promise<number> => {
-  try {
-    const response = await fetch(`${SERVER_URL}/${id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(updatedTodo),
-    });
-    return handleResponse(response).status;
-  } catch (error) {
-    console.error(RES_TODO_ERROR.EDIT, error);
-  }
+): Promise<string> => {
+  return (await axios.put(`${SERVER_URL}/${id}`, updatedTodo)).statusText;
 };
 
-const deleteTodo = async (id: number): Promise<number> => {
-  try {
-    const response = await fetch(`${SERVER_URL}/${id}`, {
-      method: 'DELETE',
-    });
-    return handleResponse(response).status;
-  } catch (error) {
-    console.error(RES_TODO_ERROR.DELETE, error);
-  }
+const deleteTodo = async (id: number): Promise<string> => {
+  return (await axios.delete(`${SERVER_URL}/${id}`)).statusText;
 };
 
 const getTodoById = async (id: number): Promise<ITodoData> => {
-  try {
-    const response = await fetch(`${SERVER_URL}/${id}`);
-    return await handleResponse(response).json();
-  } catch (error) {
-    console.error(RES_TODO_ERROR.GET_BY_ID, error);
-  }
+  return (await axios(`${SERVER_URL}/${id}`)).data;
 };
 
 export const apiService = {
